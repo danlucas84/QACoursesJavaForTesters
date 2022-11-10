1ef0aee1c078d7a1ebe63becddb3f75026e5ebaf
dan <daniel.dziuba@capgemini.com>
2022-11-10 15:30:01 +0100

    adding calculating square in java  to show how variables works

--- sandbox/src/main/java/pl/qa/courses/sandbox/FirstProgram.java
+++ sandbox/src/main/java/pl/qa/courses/sandbox/FirstProgram.java
@@ -3,8 +3,16 @@ package pl.qa.courses.sandbox;
 public class FirstProgram {
 
   public static void main(String[] args) {
-
+    String somebody = "Dan";
+    System.out.println("Hello " + somebody);
     System.out.println("Hello Word");
+    System.out.println("Hello " + "World");
+
+    double bok = 5;
+    //double bokB = 5;
+    double PoleKwadratu = bok * bok;
+
+    System.out.println("pole kwadratu dla bok = " + bok + " wynosi " + PoleKwadratu);
   }
 
 }
